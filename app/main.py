a9fe67661a2f82f4d0aa732fddbbccdd09c1ddf7
MiKPo4eLiK <stassojcuk@gmail.com>
2025-05-08 19:56:32 +0300

    result

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ class CarWashStation:
         for car in cars:
             if car.clean_mark < self.clean_power:
                 income.append(self.wash_single_car(car))
-            return round(sum(income), 1)
+        return round(sum(income), 1)
 
     def calculate_washing_price(self, car: Car) -> float:
         return round((car.comfort_class * (self.clean_power - car.clean_mark) *
